2abcfd04385db9110c9076ea47a8b40ca3e13038
Don E <DRElliston@users.noreply.github.com>
2026-05-08 05:20:59 -0500

    deploy funnel preview

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -619,7 +619,7 @@ async function createOrderViaWallet(confirmationToken, paymentMethodId) {
         ?.getAttribute("data-shipping-profile-id") || undefined;
 
   const orderData = {
-    pageId: "YVF1fWIAzn_3ZAdLMp6R9ibxbmG8SSNO4qRQgRH8N4y1ooP9satu1HzbOzSXEQUP",
+    pageId: "wmxn5upa_BdX3ndVBOi4S7Ium4dOYQvoA0-IFk4QYjHV5Bdr_mivk2sAXQQ6MKdD",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1,
@@ -1415,7 +1415,7 @@ async function createOrderViaPaypal(isExpress = false) {
   const shippingProfileId = +document.querySelector(`[data-product-id="${selectedProduct.id}"]`)?.getAttribute('data-shipping-profile-id') || undefined;
   const sameAddress = isSameAddress();
   const orderData = {
-    pageId: "YVF1fWIAzn_3ZAdLMp6R9ibxbmG8SSNO4qRQgRH8N4y1ooP9satu1HzbOzSXEQUP",
+    pageId: "wmxn5upa_BdX3ndVBOi4S7Ium4dOYQvoA0-IFk4QYjHV5Bdr_mivk2sAXQQ6MKdD",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1, // VRIO URL ending /connection
@@ -1715,7 +1715,7 @@ async function createOrderViaKlarna() {
   const sameAddress = isSameAddress();
 
   const orderData = {
-    pageId: "YVF1fWIAzn_3ZAdLMp6R9ibxbmG8SSNO4qRQgRH8N4y1ooP9satu1HzbOzSXEQUP",
+    pageId: "wmxn5upa_BdX3ndVBOi4S7Ium4dOYQvoA0-IFk4QYjHV5Bdr_mivk2sAXQQ6MKdD",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1,
     email: email,
@@ -2094,7 +2094,7 @@ async function createOrderViaCreditCard() {
   let orderTotal = Math.max(0, Number(selectedProduct.price) * selectedProduct.quantity);
 
   const orderData = {
-    pageId: "YVF1fWIAzn_3ZAdLMp6R9ibxbmG8SSNO4qRQgRH8N4y1ooP9satu1HzbOzSXEQUP",
+    pageId: "wmxn5upa_BdX3ndVBOi4S7Ium4dOYQvoA0-IFk4QYjHV5Bdr_mivk2sAXQQ6MKdD",
     action: "process",
     campaign_id: CAMPAIGN_ID,
     connection_id: 1, // VRIO URL ending /connection
@@ -4492,7 +4492,7 @@ async function returnPaypal() {
 ;
 
     const body = {
-        pageId: "YVF1fWIAzn_3ZAdLMp6R9ibxbmG8SSNO4qRQgRH8N4y1ooP9satu1HzbOzSXEQUP",
+        pageId: "wmxn5upa_BdX3ndVBOi4S7Ium4dOYQvoA0-IFk4QYjHV5Bdr_mivk2sAXQQ6MKdD",
         action: "process",
         campaign_id: CAMPAIGN_ID,
         connection_id: 1,
